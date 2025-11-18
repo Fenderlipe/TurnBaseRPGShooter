5821364959debe0cebb6d6ce369521e567a1140a
Fenderlipe <fenderlipe2004@gmail.com>
2025-11-18 10:22:40 +0100

    cosas

--- Assets/Scrips/Character.cs
+++ Assets/Scrips/Character.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    [Header("character name")]
+    [SerializeField] string name;
+    public int level = 1;
+    float currentLife;
+    [Header("character Stats")]
+    [SerializeField] float maxLife;
+    [SerializeField] float baseAttackDamage;
+    [SerializeField] float armorValue;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        name = gameObject.name;
+        currentLife = maxLife;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void TakeDamage(float damage)
+    {
+        float finalDamage = damage - armorValue;
+        currentLife -= finalDamage;
+        IsAlive();
+    }
+
+    void IsAlive()
+    {
+        if (currentLife <= 0)
+        {
+            Debug.Log(name + " Ha muerto");
+            Destroy(gameObject);
+        }
+    }
+
 }
